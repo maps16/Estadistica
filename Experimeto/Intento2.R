bf9a585f3c6145e2a47d4e9f288ed56faf691fa2
Martin Alejandro Paredes Sosa <mapsjr16@gmail.com>
2016-04-04 06:14:21 -0700

    subir archivos

--- Experimeto/Intento2.R
+++ Experimeto/Intento2.R
@@ -1,24 +1,14 @@
 ################################################################
-#LECTURA DE DATOS                                              #
+#VARIABLE ALEATORIA CONTINUA: CASO GEOMETRICA                      #
 ################################################################
 #Librerias utilizadas
 library (gdata)
 #Leyendo los datos obtenidos
 dat=read.table("Data.txt")
-
-
-#IntenatarPoisson
-################################################################
-#VARIABLE ALEATORIA DISCRETA: CASO POISSON                    #
-################################################################
-#DATOS 
+#DATOS SIMULADOS
 Xobs = dat$V1
 Xobs
-barplot(table(Xobs))
-
-#DATOS SIMULADOS
-n<-100
-
+n=100
 
 #ESTADISTICA DESCRIPTIVA
 summary(Xobs)
@@ -35,12 +25,12 @@ names(TablaFrecuencia)<-ValoresX
 barplot(TablaFrecuencia,xlab="X",ylab="Frecuencia absoluta")
 
 #ESTIMADOR DE MAXIMA VEROSIMILITUD
-EMV<-mean(Xobs)
+EMV =((1/n)*sum(Xobs))**(-1)
 EMV
 
 #GRAFICA: DENSIDAD DE PROBABILIDAD VERSUS LA ESTIMADA
 FrecuenciaRelativa<-as.numeric(prop.table(TablaFrecuencia))
-DensidadProbabilidadEMV<-dpois(ValoresX,EMV)
+DensidadProbabilidadEMV<-dgeom(ValoresX,EMV)
 plot(ValoresX,FrecuenciaRelativa,pch=19,col=1,xlab="X",ylab="Frecuencia relativa",
      main="Densidad de Probabilidad")
 points(ValoresX,DensidadProbabilidadEMV,pch=17,col=4)
@@ -48,26 +38,15 @@ legend("topright",c("Observada","Estimada"),pch=c(19,17),col=c(1,4),bty="n")
 
 #GRAFICA: DISTRIBUCION EMPIRICA VERSUS ESTIMADA
 Fempirica<-ecdf(Xobs)
-DistribucionEMV<-ppois(ValoresX,EMV)
+DistribucionEMV<-pgeom(ValoresX,EMV)
 plot(Fempirica,main="Funcion de probabilidad acumulada")
 points(ValoresX,DistribucionEMV,type="p",pch=17,col=4)
 legend("topleft",c("Observada","Estimada"),pch=c(19,17),col=c(1,4),bty="n")
 
 #GRAFICA: CUANTIL EMPIRICO VERSUS TEORICO ESTIMADO
 valpha<-(seq(1,n,1)-0.5)/n
-QTE<-qpois(valpha,EMV)
+QTE<-qgeom(valpha,EMV)
 QE<-sort(Xobs)
 plot(QTE,QE,type="p",pch=19,cex=1,xlab="Cuantil teorico estimado",ylab="Cuantil empirico",
      main="Garfica cuantil-cuantil")
 segments(4,4,8,8,lwd=2,col=4)
-
-
-
-
-
-
-
-
-
-
-
--- Experimeto/Intento3.R
+++ Experimeto/Intento3.R
@@ -1,31 +1,21 @@
 ################################################################
-#LECTURA DE DATOS                                              #
+#VARIABLE ALEATORIA DISCRETA: CASO BINOMIAL                    #
 ################################################################
+#DATOS SIMULADOS
+
 #Librerias utilizadas
 library (gdata)
 #Leyendo los datos obtenidos
 dat=read.table("Data.txt")
-
-
-#IntenatarPoisson
-################################################################
-#VARIABLE ALEATORIA DISCRETA: CASO POISSON                    #
-################################################################
-#DATOS 
+#DATOS SIMULADOS
 Xobs = dat$V1
 Xobs
-barplot(table(Xobs))
-
-#DATOS SIMULADOS
-n<-100
-
+N<-8
+p<-0.5
+k<-100
 
 #ESTADISTICA DESCRIPTIVA
 summary(Xobs)
-var(Xobs)
-r = var(Xobs)
-sd = r**(1/2)
-sd
 
 #GRAFICA DE LOS DATOS: FRECUENCIA ABSOLUTA
 ExtremoIntervalos<-seq(3.5,8.5,1)
@@ -35,12 +25,12 @@ names(TablaFrecuencia)<-ValoresX
 barplot(TablaFrecuencia,xlab="X",ylab="Frecuencia absoluta")
 
 #ESTIMADOR DE MAXIMA VEROSIMILITUD
-EMV<-mean(Xobs)
+EMV<-sum(Xobs)/(N*k)
 EMV
 
 #GRAFICA: DENSIDAD DE PROBABILIDAD VERSUS LA ESTIMADA
 FrecuenciaRelativa<-as.numeric(prop.table(TablaFrecuencia))
-DensidadProbabilidadEMV<-dpois(ValoresX,EMV)
+DensidadProbabilidadEMV<-dbinom(ValoresX,size=N,prob=EMV)
 plot(ValoresX,FrecuenciaRelativa,pch=19,col=1,xlab="X",ylab="Frecuencia relativa",
      main="Densidad de Probabilidad")
 points(ValoresX,DensidadProbabilidadEMV,pch=17,col=4)
@@ -48,26 +38,15 @@ legend("topright",c("Observada","Estimada"),pch=c(19,17),col=c(1,4),bty="n")
 
 #GRAFICA: DISTRIBUCION EMPIRICA VERSUS ESTIMADA
 Fempirica<-ecdf(Xobs)
-DistribucionEMV<-ppois(ValoresX,EMV)
+DistribucionEMV<-pbinom(ValoresX,size=N,prob=EMV)
 plot(Fempirica,main="Funcion de probabilidad acumulada")
 points(ValoresX,DistribucionEMV,type="p",pch=17,col=4)
 legend("topleft",c("Observada","Estimada"),pch=c(19,17),col=c(1,4),bty="n")
 
 #GRAFICA: CUANTIL EMPIRICO VERSUS TEORICO ESTIMADO
-valpha<-(seq(1,n,1)-0.5)/n
-QTE<-qpois(valpha,EMV)
+valpha<-(seq(1,k,1)-0.5)/k
+QTE<-qbinom(valpha,size=N,prob=EMV)
 QE<-sort(Xobs)
 plot(QTE,QE,type="p",pch=19,cex=1,xlab="Cuantil teorico estimado",ylab="Cuantil empirico",
      main="Garfica cuantil-cuantil")
 segments(4,4,8,8,lwd=2,col=4)
-
-
-
-
-
-
-
-
-
-
-
